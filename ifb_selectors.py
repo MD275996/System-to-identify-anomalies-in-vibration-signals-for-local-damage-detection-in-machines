77b436bc2096f5bd95857d6bfab72f8c8e385fac
Marek Dworaczyk <275996@student.pwr.edu.pl>
2025-10-19 08:33:12 +0200

    sk, jb, kss

--- ifb_selectors.py
+++ ifb_selectors.py
@@ -3,27 +3,43 @@ import scipy
 from scipy.signal import stft
 
 def SK(signal, fs):
-    array_freq, array_tt, matrix_Zxx =stft(signal, fs = 1000, window = 'hann')
+    array_freq, array_tt, matrix_Zxx =stft(signal, fs = fs, window = 'hann')
     Zxx = np.abs(matrix_Zxx)
-    abs_Zxx = np.abs(Zxx)
-    num = np.sum(np.power(abs_Zxx[f:],4))
-    denum = np.sum(np.power(abs_Zxx[f:],2))
-    T = len(t)
-    return (T*num/denum)-2 
+    
+    sk_results = np.zeros_like(array_freq)
+    T = len(array_tt)
 
-def JB(Zxx, f, t):
-    abs_Zxx = np.abs(Zxx)
-    s = scipy.stats.skew(abs_Zxx[f])
-    k = scipy.stats.kurtosis(abs_Zxx[f])
-    T = len(t)
-    print(f"Skewness: {s}, kurtosis: {k}")
-    return T/6 * (s**2 + (np.power(k-1,2)/4))
+    for f in range(0,len(array_freq)):
+        num = np.sum(np.power(Zxx[f,:],4))
+        denum = np.sum(np.power(Zxx[f,:],2))
+        sk_results[f] = (T*num/denum) - 2
 
-def KSS(Zxx,f,t):
-    abs_Zxx = np.abs(Zxx)
-    mean_signal = np.mean(abs_Zxx[f])
-    std_signal = np.std(abs_Zxx[f])
-    cdf_sample = np.zeros_like(t)
-    for i in range(len(t)):
-        cdf_sample[i] = scipy.stats.norm.cdf(t[i],loc=mean_signal, scale=std_signal)
-    return(scipy.stats.kstest(abs_Zxx[f],cdf_sample))
+    return sk_results
+
+
+def JB(signal, fs):
+    array_freq, array_tt, matrix_Zxx =stft(signal, fs = fs, window = 'hann')
+    Zxx = np.abs(matrix_Zxx)
+    T = len(array_tt)
+    jb_results = np.zeros_like(array_freq)
+
+    for f in range(len(array_freq)):
+        s = scipy.stats.skew(Zxx[f])    
+        k = scipy.stats.kurtosis(Zxx[f])
+        jb_results[f] = T/6 * (s**2 + (np.power(k-1,2)/4))
+    
+    return jb_results
+
+def KSS(signal, fs):
+    array_freq, array_tt, matrix_Zxx =stft(signal, fs = fs, window = 'hann')
+    Zxx = np.abs(matrix_Zxx)
+    kss_results = np.zeros_like(array_freq)
+
+    for f in range(len(array_freq)):
+        mean_signal = np.mean(Zxx[f])
+        std_signal = np.std(Zxx[f])
+        cdf_sample = np.zeros_like(array_tt)
+        for i in range(len(array_tt)):
+            cdf_sample[i] = scipy.stats.norm.cdf(array_tt[i],loc=mean_signal, scale=std_signal)
+        kss_results[f] = scipy.stats.kstest(Zxx[f],cdf_sample).pvalue
+        
