19bd5c57d34e43adf32f746dd46293aab31edb90
Marek Dworaczyk <275996@student.pwr.edu.pl>
2025-10-20 09:53:13 +0200

    some changes

--- ifb_selectors.py
+++ ifb_selectors.py
@@ -4,14 +4,13 @@ import matplotlib.pyplot as plt
 from scipy.signal import stft
 from scipy.stats import norm
 
-def SK(signal, fs):
-    array_freq, array_tt, matrix_Zxx =stft(signal, fs = fs, window = 'hann')
-    Zxx = np.abs(matrix_Zxx)
-    
-    sk_results = np.zeros_like(array_freq)
-    T = len(array_tt)
+def SK(Zxx):
+    F = Zxx.shape[0]
+    T = Zxx.shape[1]
+    #wyrysować spektrogram
+    sk_results = np.zeros(F)
 
-    for f in range(0,len(array_freq)):
+    for f in range(F):
         num = np.sum(np.power(Zxx[f,:],4))
         denum = np.sum(np.power(Zxx[f,:],2))
         sk_results[f] = (T*num/denum) - 2
@@ -19,41 +18,39 @@ def SK(signal, fs):
     return sk_results
 
 
-def JB(signal, fs):
-    array_freq, array_tt, matrix_Zxx =stft(signal, fs = fs, window = 'hann')
-    Zxx = np.abs(matrix_Zxx)
-    T = len(array_tt)
-    jb_results = np.zeros_like(array_freq)
+def JB(Zxx):
+    F = Zxx.shape[0]
+    T = Zxx.shape[1]
+    jb_results = np.zeros(F)
 
-    for f in range(len(array_freq)):
+    for f in range(F):
         s = scipy.stats.skew(Zxx[f])    
         k = scipy.stats.kurtosis(Zxx[f])
         jb_results[f] = T/6 * (s**2 + (np.power(k-1,2)/4))
     
     return jb_results
 
-def KSS(signal, fs, alpha):
-    array_freq, array_tt, matrix_Zxx =stft(signal, fs = fs, window = 'hann')
-    Zxx = np.abs(matrix_Zxx)
-    kss_results = np.zeros_like(array_freq)
-
-    for f in range(len(array_freq)):
+def KSS(Zxx):
+    F = Zxx.shape[0]
+    T = Zxx.shape[1]
+    kss_results = np.zeros(F)
+    for f in range(F):
         mean_signal = np.mean(Zxx[f])
         std_signal = np.std(Zxx[f])
-        cdf_sample = np.zeros_like(array_tt)
-        for i in range(len(array_tt)):
-            cdf_sample[i] = scipy.stats.norm.cdf(array_tt[i],loc=mean_signal, scale=std_signal)
+        cdf_sample = np.zeros(T)
+        for i in range(T):
+            cdf_sample[i] = scipy.stats.norm.cdf(Zxx[f,i],loc=mean_signal, scale=std_signal)
         test_stats = scipy.stats.kstest(Zxx[f],cdf_sample)
-        kss_results[f] = np.int64(test_stats.pvalue < alpha)
+        kss_results[f] = 1/test_stats.statistic
     
     return kss_results
 
 
-def AD(signal, fs):
-    array_freq, array_tt, matrix_Zxx =stft(signal, fs = fs, window = 'hann')
-    Zxx = np.abs(matrix_Zxx)
-    ad_results = np.zeros_like(array_freq)
-    for f in range(len(array_freq)):
+def AD(Zxx):    
+    F = Zxx.shape[0]
+    T = Zxx.shape[1]
+    ad_results = np.zeros(F)
+    for f in range(F):
         test_result = scipy.stats.anderson(Zxx[f], dist='norm')
-        ad_results[f] = np.int64(test_result.statistic < test_result.critical_values[2])     
+        ad_results[f] = test_result.statistic 
     return ad_results  
