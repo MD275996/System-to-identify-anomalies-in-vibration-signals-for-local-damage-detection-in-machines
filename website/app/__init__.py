cef54b227f317b604226af3913b8839170b44970
Marek Dworaczyk <275996@student.pwr.edu.pl>
2025-12-19 18:53:36 +0100

    feat: clearing tmp

--- website/app/__init__.py
+++ website/app/__init__.py
@@ -1,6 +1,17 @@
 from flask import Flask
+import os
+import shutil
+
+TMP_DIR = "app/static/tmp"
+
+def clear_tmp():
+    if os.path.exists(TMP_DIR):
+        shutil.rmtree(TMP_DIR)
+    os.makedirs(TMP_DIR, exist_ok=True)
 
 def create_app():
+    clear_tmp()
+
     app = Flask(__name__)
 
     #import blueprintów
